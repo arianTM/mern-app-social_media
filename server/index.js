940c2fff3b85eb2db9f3063497ef4bb056b20872
arianTM <88108832+arianTM@users.noreply.github.com>
2023-04-23 16:38:08 -0500

    Connecting to the Database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,37 @@
+// DEPENDENCIES
 import express from "express";
-import bodyParser from "body—parser";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
+// IMPORT ENV VARIABLES
+dotenv.config();
+
+// APP: server
 const app = express();
+
+// MIDDLEWARES
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// DATABASE INFO
+const CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// CONNECT TO DATABASE
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
